6c5e519ba7f895faee94c151ab3dee9e471349d5
Travis Beck <tjbenator@gmail.com>
2012-05-21 16:54:17 -0700

    First Commit

---
+++ Playlistmaker.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+########################
+#V1.1
+#Created by: Travis Beck
+########################
+#Folder name for playlists!
+#playlists="CHANGEME"
+playlists="/home/travis/playlists"
+
+#Audio player for -p arg. This is used to play the playlist after it is created
+#eval $PLAYER $PLAYLIST
+PLAYER="vlc"
+
+#If needed later
+#script=`readlink -f $0`
+#script_path=`dirname $script`
+
+EXT="*.MP3"
+
+#Time and Date Format
+date=`date +%Y-%m-%d`
+
+#===================================================================================
+#End of user configuration
+#===================================================================================
+
+if [ "$playlists" = "CHANGEME" ]; then
+	echo "Please configure!"; exit 0
+fi 
+
+#Make everything fancy
+colorize()
+{
+        color="\e[0;34m"
+        case $1 in
+        "red")
+                color="\e[0;35m";;
+        "green")
+                color="\e[0;32m";;
+        "yellow")
+                color="\e[0;33m";;
+        "blue")
+                color="\e[0;36m";;
+        esac
+        echo -e "$color$2\e[0m"
+}
+
+generate()
+{
+	#Make script case insensitive
+	shopt -s nocaseglob
+	mkdir -p $playlists
+	if [ -d "$@" ]; then
+		pushd "$@" >> /dev/null
+			currentdir="$@"
+			playlist_name=`basename "$PWD"`.m3u
+			playlist="$playlists/$playlist_name"
+
+			if [ -f "$playlist" ]; then
+				colorize red "Removing playlist with same name"
+				rm "$playlist"
+			fi
+
+			for file in $EXT; do
+				if [ "$file" != "*.MP3" ] || [ "$file" != "*.WAV" ] ; then
+				
+					#First time create file
+					if [ ! -f "$playlist" ] ;
+					then
+					    #colorize red "Creating Playlist file!"
+					    echo "#EXTM3U" > "$playlist"
+					fi
+					colorize yellow "+ Adding \"$file\" to playlist"
+					echo "#EXTINF:123,$file"  >> "$playlist"
+					echo "$currentdir$file" >> "$playlist"
+				fi
+		done
+	else
+		colorize red "Correct usage:"
+		colorize red "playlistmaker [directory]"
+	fi
+
+	#Rewind
+	popd >> /dev/null
+	#Make case sesitive
+	shopt -u nocaseglob
+
+	#Done
+	colorize green "\"$playlist\" has been created!"
+}
+
+#MANAGE COMMANDLINE ARGS
+RECUR="FALSE"
+PLAY="FALSE"
+while getopts rp OPTION
+do
+         case $OPTION in
+          r) RECUR="TRUE" ;;
+	  p) PLAY="TRUE"
+         esac
+done
+shift $(( OPTIND - 1 ))  # shift past the last flag or argument
+
+PARAM=$*
+
+###########################################################################
+#MAIN
+###########################################################################
+
+
+if [ "$RECUR" = "FALSE" ]; then
+	#Default Mode
+	colorize green "Generating $PARAM"
+	generate "$PARAM"
+else
+	colorize blue "NEXT VERSION! STAY TUNED!"
+fi
+
+if [ "$PLAY" = "TRUE" ]; then
+	colorize blue "PLAY: $PLAYER $playlist"
+	#Play and release terminal
+	eval nohup $PLAYER "'$playlist'" 2>/dev/null 1>/dev/null &
+fi
+
+echo 
+colorize yellow "BYE! ENJOY YOUR PLAYLIST! YOU LAZY BASTARD :D"
+echo 
